bace2f14e640eef43175d25c5b3b23075e0b02e5
NickChubb <nick@nickchubb.ca>
2021-12-15 21:27:08 -0800

    Fix errors

--- pages/api/ipfs.js
+++ pages/api/ipfs.js
@@ -1,43 +1,43 @@
-import * as IPFS from 'ipfs-core'
-import { cidToUrl } from '@Helpers/ipfs';
+// import * as IPFS from 'ipfs-core'
+// import { cidToUrl } from '@Helpers/ipfs';
 
-export default function handler (req, res) {
+// export default function handler (req, res) {
 
-    // POST method uploads request data to IPFS
-    if (req.method === 'POST') {
+//     // POST method uploads request data to IPFS
+//     if (req.method === 'POST') {
 
-        // Initialize IPFS
-        const ipfs = await IPFS.create();
+//         // Initialize IPFS
+//         const ipfs = await IPFS.create();
 
-        // Upload Image to IPFS
-        const image = await ipfs.add()
+//         // Upload Image to IPFS
+//         const image = await ipfs.add()
 
-        // Create JSON document
-        const metadata = {
-            "name": req.body.name,
-            "symbol": req.body.symbol,
-            "description": req.body.description,
-            "size": req.body.size,
-            "image": cidToUrl(image.cid),
-            "visibility": req.body.visibility
-        }
+//         // Create JSON document
+//         const metadata = {
+//             "name": req.body.name,
+//             "symbol": req.body.symbol,
+//             "description": req.body.description,
+//             "size": req.body.size,
+//             "image": cidToUrl(image.cid),
+//             "visibility": req.body.visibility
+//         }
 
-        // Upload JSON file to IPFS
-        const { cid } = await ipfs.add(new File(
-            metadata, 
-            'metadata.json'));
+//         // Upload JSON file to IPFS
+//         const { cid } = await ipfs.add(new File(
+//             metadata, 
+//             'metadata.json'));
 
-        res.status(200).send( cidToUrl(cid) );
+//         res.status(200).send( cidToUrl(cid) );
         
-    } else {
-      // Handle any other HTTP method
-    }
-}
+//     } else {
+//       // Handle any other HTTP method
+//     }
+// }
 
-export const config = {
-    api: {
-      bodyParser: {
-        sizeLimit: '10mb',
-      },
-    },
-  }
+// export const config = {
+//     api: {
+//       bodyParser: {
+//         sizeLimit: '10mb',
+//       },
+//     },
+//   }
